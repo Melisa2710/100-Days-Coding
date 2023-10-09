8879fffb53df8eccc14a8852246d15057b8e1587
Melisa2710 <melisamel278@gmail.com>
2023-10-09 04:15:40 -0700

    first commit

---
+++ Day 1/src/day/pkg1/Day1.java
@@ -0,0 +1,12 @@
+
+package day.pkg1;
+
+public class Day1 {
+
+    public static void main(String[] args) {
+        System.out.println("Day 1");
+        
+     //ini adalah komentar
+    }
+    
+}
