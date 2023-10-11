68b9a2643b1a71bb1599507e1a3016d021f8010a
Melisa2710 <147118007+Melisa2710@users.noreply.github.com>
2023-10-11 04:28:03 -0700

    Add files via upload

---
+++ Day3.java
@@ -0,0 +1,23 @@
+
+package day3;
+
+public class Day3 {
+
+    public static void main(String[] args) {
+        
+     String nama = "Melisa";
+     String nim = "D0223302";
+     String kelas = "Informatika a";
+     String fakultas = "Teknik";
+     String prodi = "Informatika";
+     String universitas = "Sulawesi Barat";
+     
+      System.out.println("nama : "+nama);
+      System.out.println("nim : "+nim);
+      System.out.println("kelas : "+kelas);
+      System.out.println("fakultas : "+fakultas);
+      System.out.println("prodi : "+prodi);
+      System.out.println("universitas : "+universitas);
+    }
+    
+}
