1d005aa4927b550d08cda3b7378f5be341c406df
Melisa2710 <147118007+Melisa2710@users.noreply.github.com>
2023-10-12 02:24:00 -0700

    Add files via upload

---
+++ Day4.java
@@ -0,0 +1,13 @@
+
+package day4;
+
+public class Day4 {
+
+    public static void main(String[] args) {
+        //Day 4
+        
+        int angka = 5; angka*=3;
+        System.out.println(angka);
+    }
+    
+}
