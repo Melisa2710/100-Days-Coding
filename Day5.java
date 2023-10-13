b822247aa4f20361687556d924cc1b8cd4ccb6bc
Melisa2710 <147118007+Melisa2710@users.noreply.github.com>
2023-10-13 04:19:44 -0700

    Add files via upload

---
+++ Day5.java
@@ -0,0 +1,15 @@
+
+package day5;
+
+public class Day5 {
+
+    public static void main(String[] args) {
+        //Day5
+        
+        String nama = "Melisa";
+        char kelas = 'A';
+        
+        System.out.println("Nama\t: " + nama + "\n" + "kelas\t: " + kelas);
+    }
+    
+}
