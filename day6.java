d1b02d8cc0d61aadd76c85cdaa7b9fd21db7062e
Melisa2710 <147118007+Melisa2710@users.noreply.github.com>
2023-10-14 01:37:45 -0700

    Add files via upload

---
+++ day6.java
@@ -0,0 +1,14 @@
+
+package pkg100days;
+
+public class day6 {
+    public static void main(String[] args) {
+       // System.out.println("Hellow word");
+        long angka1,angka2;
+        angka1 = 20;
+        angka2 = 25;
+        System.out.println(angka1 != angka2);
+        
+    }
+    
+}
