a8dfd1c27b49d40876ec187640a7db99dda2aba7
Melisa2710 <147118007+Melisa2710@users.noreply.github.com>
2023-10-17 04:55:48 -0700

    Add files via upload

---
+++ Day9.java
@@ -0,0 +1,13 @@
+
+package pkg100days;
+
+public class Day9 {
+    public static void main(String[] args) {
+      String nama = "Melisa";
+      double tinggi_badan = 140.5;
+       
+      System.out.println(nama+tinggi_badan);
+    }
+    
+          
+}
