21c5fe8b11d9a7c211de78fc6d11df43e83b19e4
Melisa2710 <147118007+Melisa2710@users.noreply.github.com>
2023-10-18 01:55:08 -0700

    Day10.java

--- Day10.java
+++ Day10.java
@@ -4,8 +4,8 @@ package pkg100days;
 public class Day10 {
     public static void main(String[] args) {
        String nama = "Melisa";
-       float beratbadan = 45.8f;
-        System.out.print("Nama : "+nama+ "\n" +"beratbadan : "+beratbadan);
+       float berat_badan = 45.8f;
+        System.out.print("Nama : "+nama+ "\n" +"berat_badan : "+berat_badan);
     }
   
 }
