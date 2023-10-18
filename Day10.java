1ddfcb3ef22662c223aa9ced957dffa624d411aa
Melisa2710 <147118007+Melisa2710@users.noreply.github.com>
2023-10-18 01:51:42 -0700

    Add files via upload

---
+++ Day10.java
@@ -0,0 +1,11 @@
+
+package pkg100days;
+
+public class Day10 {
+    public static void main(String[] args) {
+       String nama = "Melisa";
+       float beratbadan = 45.8f;
+        System.out.print("Nama : "+nama+ "\n" +"beratbadan : "+beratbadan);
+    }
+  
+}
