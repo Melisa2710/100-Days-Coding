e783c236a2efafb5463b5df71effe585bf767291
Melisa2710 <147118007+Melisa2710@users.noreply.github.com>
2023-10-19 03:52:18 -0700

    Add files via upload

---
+++ Day11.java
@@ -0,0 +1,23 @@
+
+package pkg100days;
+
+
+public class Day11 {
+    public static void main(String[] args) {
+       
+        //harga pc
+        double harga_pc_gaming = 150000000;
+       
+        //batas tabungan
+        double maksimal_tabungan = 5000000;
+        
+        
+        double bulan_yang_dibutuhkan_untuk_menabung = harga_pc_gaming/maksimal_tabungan;
+        double jumlah_bulan_dalam_satu_tahun = 12;
+        
+        System.out.println("Tahun yang dibutuhkan sambo untuk membeli Pc gaming : " + (jumlah_bulan_dalam_satu_tahun)+ "Tahun" );
+        
+        
+    }
+  
+}
