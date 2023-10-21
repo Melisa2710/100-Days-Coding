2da10e169db7cffbd83761898341df19a823e036
Melisa2710 <147118007+Melisa2710@users.noreply.github.com>
2023-10-21 23:05:53 +0800

    Day13.java
    Type data Primitif

---
+++ Day13.java
@@ -0,0 +1,11 @@
+package Day13;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        String nama = "Melisa";
+        short umur = 17;
+        System.out.println("Nama : "+nama+ "\n" +"umur : "+umur);
+    }
+    
+         }
