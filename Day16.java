139dec6db2759c57763a0f15cd97ddb47e920e2d
Melisa2710 <147118007+Melisa2710@users.noreply.github.com>
2023-10-24 13:47:21 +0800

    Day16.java
    Type data int

---
+++ Day16.java
@@ -0,0 +1,13 @@
+package Day16;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        int angka = 2;
+        angka*=3;
+        
+        
+       System.out.println(angka!=angka);
+    }
+    
+}
