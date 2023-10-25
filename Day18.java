3b8cfa47c8cb6ccafd830109cb77a8242c46fa0b
Melisa2710 <147118007+Melisa2710@users.noreply.github.com>
2023-10-26 04:39:26 +0800

    Day18.java
    Type data primitif (boolean)

---
+++ Day18.java
@@ -0,0 +1,12 @@
+package Day18;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        int nilai = 3;
+        int angka = 5;
+        
+         System.out.println(angka<nilai);
+    }
+    
+}
