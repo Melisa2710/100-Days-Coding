134b01fd74f22507d00930d5c7ea84b83402ddd6
Melisa2710 <147118007+Melisa2710@users.noreply.github.com>
2023-10-27 07:59:41 +0800

    Day19.java
    Percabangan

---
+++ Day19.java
@@ -0,0 +1,15 @@
+package Day19;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+     if(true){
+      System.out.println("awal");
+    }else{
+      System.out.println("akhir");
+    
+   }
+    }
+}
+    
