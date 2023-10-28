a3968008711de6e8e3bba7a732cd165b2383479d
Melisa2710 <147118007+Melisa2710@users.noreply.github.com>
2023-10-28 14:03:10 +0800

    Day20.java
    Percabangan

---
+++ Day20.java
@@ -0,0 +1,13 @@
+package Day20;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        if(true){
+          System.out.println("awal");
+        }else{
+           System.out.println("akhir");
+    }
+    
+}
+}
