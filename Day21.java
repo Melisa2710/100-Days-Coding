9c23df5fb8204e755d7748733b28c5d8c533183f
Melisa2710 <147118007+Melisa2710@users.noreply.github.com>
2023-10-29 16:20:06 +0800

    Day21.java
    Perulangan

---
+++ Day21.java
@@ -0,0 +1,12 @@
+package Day21;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        for(int a=2; a<20; a+=4){
+          System.out.println(a);
+    }
+    
+}
+}
