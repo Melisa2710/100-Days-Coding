6d54b5d1f67d1f2a9b68fc1a917e016eb43e8439
Melisa2710 <147118007+Melisa2710@users.noreply.github.com>
2023-10-30 18:56:35 +0800

    Day22.java
    Percabangan

---
+++ Day22.java
@@ -0,0 +1,14 @@
+package Day22;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        int angka = 2;
+        if(angka>2){
+            System.out.println("Salah");
+        }else if(angka > 3){
+            System.out.println("Benar");
+    }
+    
+}
+}
