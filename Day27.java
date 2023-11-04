9968405aea4363f0be9e83b266356727f753ec10
Melisa2710 <147118007+Melisa2710@users.noreply.github.com>
2023-11-05 04:26:19 +0800

    Day27.java
    Percabangan

---
+++ Day27.java
@@ -0,0 +1,34 @@
+package Day27;
+
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Masukkan Nilai Anda : ");
+        double nilai_anda = input.nextDouble();
+        
+        if(nilai_anda >=90 ){
+            System.out.print("Nilai \t\t: A ");
+        }
+        else if(nilai_anda >=80 ){
+            System.out.print("Nilai \t\t: B");
+        }
+        else if(nilai_anda >= 65 ){
+            System.out.print("Nilai \t\t: C");
+        }
+        else if(nilai_anda >=45){
+            System.out.print("Nilai \t\t: D");
+        }
+        else{
+            System.out.print("Nilai \t\t: E");
+        }
+    }  
+
+    
+        
+    }
+    
