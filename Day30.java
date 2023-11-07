5a5e4779cab5c6cac98cc2ee58a8f4dd31107a85
Melisa2710 <147118007+Melisa2710@users.noreply.github.com>
2023-11-07 21:59:23 +0800

    Day30.java
    Daftar pembelian

---
+++ Day30.java
@@ -0,0 +1,23 @@
+package Day30;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        
+        int jum_semua = total_makan + total_minum;
+        
+        System.out.println("|****************************************|");
+        System.out.println("|              DAFTAR PEMBELIAN          |");
+        System.out.println("|****************************************|");
+        System.out.println("Makanan      : " + nama_makanan);
+        System.out.println("jumlah porsi : " + jum_porsi);
+        System.out.println("Minuman      : " + nama_minum);
+        System.out.println("Jumlah gelas : " + jum_gelas);
+        System.out.println("|========================================|");
+        System.out.println("Total harga  : " + jum_semua);
+        System.out.println("|========================================|");
+        
+    }
+    
+}
