f80439b8a75b4011b2db39cdccebd33a82a5e406
Melisa2710 <147118007+Melisa2710@users.noreply.github.com>
2023-11-08 16:44:34 +0800

    Day31.java
    Type data primitif

---
+++ Day31.java
@@ -0,0 +1,19 @@
+package Day31;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        // Mendefinisikan dua variabel untuk menyimpan angka
+        int angka1 = 5;
+        int angka2 = 3;
+
+        // Menghitung penjumlahan kedua angka
+        int hasil = angka1 + angka2;
+
+        // Mencetak hasil penjumlahan
+        System.out.println("Hasil penjumlahan " + angka1 + " dan " + angka2 + " adalah " + hasil);
+    
+    }
+    
+}
