46c93af9a0c9169d6e84af90f933c4b3b9ed16f3
Melisa2710 <147118007+Melisa2710@users.noreply.github.com>
2023-11-12 19:50:32 +0800

    Day35.java
    Perulangan

---
+++ Day35.java
@@ -0,0 +1,23 @@
+package Day35;
+
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        int batas;
+
+        System.out.print("Masukkan Batas angka : ");
+        batas = scanner.nextInt();
+
+        // Perulangan
+        for (int i = 1; i <= batas; i++) {
+            
+            // Menampilkan angka
+            System.out.println(i);
+    }
+    
+  }
+}
