3c108dc1a3d3e7e1f7119cc09f80293b6f1c10cc
Melisa2710 <147118007+Melisa2710@users.noreply.github.com>
2023-11-14 20:14:53 +0800

    Day37.java
    Type data primitif

---
+++ Day37.java
@@ -0,0 +1,22 @@
+package Day37;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+       int angka1 = 12;
+       int angka2 = 27;
+        
+      boolean hasil;
+        
+       hasil = angka1!=angka2;
+        System.out.println(hasil);
+        
+       hasil = angka1==angka2;
+        System.out.println(hasil);
+        
+        
+        
+    }
+    
+}
