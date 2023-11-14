02aea01d0a4881456217aec9c410b97d412918df
Melisa2710 <147118007+Melisa2710@users.noreply.github.com>
2023-11-15 07:40:49 +0800

    Day38.java
    Type data non primitif (String)

---
+++ Day38.java
@@ -0,0 +1,15 @@
+package Day38;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        String  nama_depan, nama_belakang, nama_lengkap;
+        nama_depan = "Melisa";
+        nama_belakang = "Lisa";
+        nama_lengkap = nama_depan +" "+ nama_belakang;
+        System.out.println("Nama depan\t : " + nama_depan + "\nnama belakang\t : " + nama_belakang + "\nnama_lengkap\t : " + nama_lengkap);
+    
+    }
+    
+}
