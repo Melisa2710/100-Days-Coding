fc8d7bf9af65a7d0761b44530945d47eccd2326d
Melisa2710 <147118007+Melisa2710@users.noreply.github.com>
2023-11-21 20:15:26 +0800

    Day44.java
    Penugasan dengan operator logika

---
+++ Day44.java
@@ -0,0 +1,13 @@
+package Day44;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        boolean kondisi1 = true;
+        boolean kondisi2 = false;
+        boolean hasilLogika = kondisi1 && kondisi2; // hasilnya false, karena kondisi1 dan kondisi2 harus true untuk hasil true
+        
+        System.out.print(hasilLogika);
+    }
+    
+}
