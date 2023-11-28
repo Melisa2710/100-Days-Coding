1fd0137a70ba1fd5d36001b1c6ccbd361b376cb5
Melisa2710 <147118007+Melisa2710@users.noreply.github.com>
2023-11-28 20:20:32 +0800

    Day51.java
    Method,  operator aritmetika menggunakan (+)

---
+++ Day51.java
@@ -0,0 +1,17 @@
+package Day51;
+
+public class Main {
+   public static void operator (int a, int b){
+       int c;
+       c = a + b;
+       System.out.print(a+ " + " +b+ " = "+c);
+       
+   }
+    public static void main(String[] args) {
+        int a,b;
+        a = 30;
+        b = 15;
+        operator(a, b);
+    }
+    
+}
