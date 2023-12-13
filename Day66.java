9e3d671269a631dc2ace91950b62f700839e9de2
Melisa2710 <147118007+Melisa2710@users.noreply.github.com>
2023-12-13 12:36:11 +0800

    Day66.java
    Manipulasi string

---
+++ Day66.java
@@ -0,0 +1,14 @@
+package Day66;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        String nama = "MELISA";
+        
+        /*untuk mengetahui jumlah karakter yang ada
+        di suatu kumpulan kata atau suatu kalimat
+        */
+        System.out.print(nama.length());
+    }
+    
+}
