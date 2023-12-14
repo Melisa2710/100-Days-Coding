e915026d451107899031af87e66e46147d4bf220
Melisa2710 <147118007+Melisa2710@users.noreply.github.com>
2023-12-14 08:26:34 +0800

    Day67.java
    Manipulasi string

---
+++ Day67.java
@@ -0,0 +1,14 @@
+package Day67;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        String nama = "Melisa";
+        
+        /* Mengubah isi dari variabel nama menjadi huruf
+        kapital semua
+        */
+        System.out.print(nama.toUpperCase());
+    }
+    
+}
