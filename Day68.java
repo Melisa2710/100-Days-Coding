03b6bfc19df444d8bd0ef7c7b5d0e6e0b7078772
Melisa2710 <147118007+Melisa2710@users.noreply.github.com>
2023-12-15 17:47:12 +0800

    Day68.java
    Manipulasi string

---
+++ Day68.java
@@ -0,0 +1,14 @@
+package Day68;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        String nama = "Melisa";
+        
+        /* Mengubah isi variabel nama menjadi huruf 
+        kecil semua
+        */
+        System.out.print(nama.toLowerCase());
+    }
+    
+}
