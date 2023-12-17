f4516300addc0dd6f4733384f85f1c472e661b6a
Melisa2710 <147118007+Melisa2710@users.noreply.github.com>
2023-12-18 04:09:39 +0800

    Day70.java

---
+++ Day70.java
@@ -0,0 +1,27 @@
+package Day70;
+
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan batas angka : ");
+        int a = sc.nextInt();
+        for(int i=0;i<a;i++){
+            System.out.print("masukkan angka :");
+            int angka = sc.nextInt();
+            if(angka==0){
+                System.out.println("netral");
+            }else if(angka%2==0){
+                System.out.println("genap");
+            }else{
+                System.out.println("ganjil");
+            }
+        }
+        System.out.println("\n================");
+    
+
+    }
+    
+}
