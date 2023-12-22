058d198e4fa1113ad98af92faf9381bd4cfd0267
Melisa2710 <147118007+Melisa2710@users.noreply.github.com>
2023-12-23 05:13:02 +0800

    Day76.java

---
+++ Day76.java
@@ -0,0 +1,26 @@
+package Day76;
+
+import java.util.Scanner:
+
+public class Main {
+    
+    public static void main(String[] args) {
+       Scanner input = new Scanner(System.in);
+        
+        int nilai;
+
+        do {
+            System.out.print("Masukkan angka: ");
+            nilai = input.nextInt();
+
+            if (nilai % 2 == 0) {
+                System.out.println("angka genap, program berhenti.");
+                break;
+            } else {
+                System.out.println("angka ganjil, masukkan angka lagi.");
+            }
+        } while (true);
+
+    }
+    
+}
