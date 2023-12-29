85142a481118980b0cc62a405487adbe951f3eb7
Melisa2710 <147118007+Melisa2710@users.noreply.github.com>
2023-12-30 03:05:40 +0800

    Day83.java
    Pola segitiga dengan perulangan

---
+++ Day83.java
@@ -0,0 +1,15 @@
+package Day83;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        int jmlh = 4;
+        for(int i = 1; i <= jmlh; i++){
+            for(int j = 1; j < i; j++){
+                System.out.print("o  ");
+            }
+            System.out.println("o");
+        }
+    }
+    
+}
