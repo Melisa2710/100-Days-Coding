aebc69e7c954fa31d6b81adeaafb1def03820eb1
Melisa2710 <147118007+Melisa2710@users.noreply.github.com>
2024-01-03 02:31:57 +0800

    Day86.java
    Pola persegi

---
+++ Day86.java
@@ -0,0 +1,15 @@
+package Day86;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        int jumlah = 5;
+        for(int i = 1; i <= jumlah; i++){
+            for(int j = 1; j < jumlah;  j++){
+                System.out.print("o  ");
+            }
+          System.out.println("o  ");  
+        }
+    }
+    
+}
