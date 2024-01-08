43a3060229844c60294ce5c7edc9b5ec4cc31a75
Melisa2710 <147118007+Melisa2710@users.noreply.github.com>
2024-01-08 20:20:50 +0800

    Day92.java
    Menampilkan angka dari 77 ke 33 menggunakan perulangan

---
+++ Day92.java
@@ -0,0 +1,12 @@
+package Day92;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        for(int i = 77; i > 33; i--){
+            System.out.println(i);
+        }
+    }
+    
+}
