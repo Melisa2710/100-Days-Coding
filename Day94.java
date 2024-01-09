77b7be9464124a412418b00ae31eb1394e569490
Melisa2710 <147118007+Melisa2710@users.noreply.github.com>
2024-01-10 05:45:43 +0800

    Day94.java
    Array 2 dimensi

---
+++ Day94.java
@@ -0,0 +1,13 @@
+package Day94;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        String [][] data = {
+            {"Melisa","\t (Mamasa)"},
+            {"\nMel","\t (Mamasa)"}
+        };
+        System.out.print(data[0][0] + data[0][1] + data[1][0] + data[1][1]);
+    }
+    
+}
