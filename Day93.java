3504af60ec73da3370ba4edaf84c55ce22c9c0da
Melisa2710 <147118007+Melisa2710@users.noreply.github.com>
2024-01-10 05:43:59 +0800

    Day93.java
    Array 2 dimensi

---
+++ Day93.java
@@ -0,0 +1,15 @@
+package Day93;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        String[][] buah = new String [2][2];
+        buah [0][0] = "Mangga";
+        buah [0][1] = "Apel";
+        buah [1][0] = "Durian";
+        buah [1][1] = "Anggur";
+        
+        System.out.print(buah[0][0]);
+    }
+    
+}
