1b7664579fef3d29430f4ac6aade73929e2b09f9
Melisa2710 <147118007+Melisa2710@users.noreply.github.com>
2024-01-14 00:31:16 +0800

    Day98.java

---
+++ Day98.java
@@ -0,0 +1,14 @@
+package Day98;
+
+public class Main {
+    
+    public static void main(String[] args) {
+       int [] angka = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+        int jumlah = angka.length;
+        
+        for (int i = 0; i <=8; i++){
+            System.out.println(angka[i]);
+        }    
+    }
+    
+}
