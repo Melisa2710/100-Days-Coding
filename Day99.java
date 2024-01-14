c80099b57caf4821aa468b4df3264636a6ba0e6d
Melisa2710 <147118007+Melisa2710@users.noreply.github.com>
2024-01-15 01:11:42 +0800

    Day99.java

---
+++ Day99.java
@@ -0,0 +1,15 @@
+package Day99;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        int [] angka = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+        int jumlah = angka.length;
+        
+        for (int i = 8; i >=0; i--){
+            System.out.println(angka[i]);
+        }    
+    }
+    
+}
+//menampilkan urutan angka dari angka 1 - 9
