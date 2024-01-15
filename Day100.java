0349f12984e5472965343db9d478b116cd96fcf3
Melisa2710 <147118007+Melisa2710@users.noreply.github.com>
2024-01-15 20:15:42 +0800

    Day100.java
    Clas objek

---
+++ Day100.java
@@ -0,0 +1,14 @@
+package Day100;
+
+class angka{
+    int x = 5;
+}
+
+public class Main {
+    
+    public static void main(String[] args) {
+        angka objek = new angka();
+        System.out.print(objek.x+5);
+    }
+    
+}
